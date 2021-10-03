54501206cf662b8b431f0595f0feaa64eec7e67e
Roesn <Bubwub04@icloud.com>
2021-10-03 19:31:12 -0400

    ex30skele

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+public class Solution30 {
+    /*
+    * make method to create a multiplication table 'multTable'
+        * declare integer variables 'i' and 'j'
+        * run for loop for num row starting at 1; through 12; increment each loop
+            * run for loop for num colomns starting at 1; through 12; increment each loop
+            * print ''i' * 'j''
+        * print new line
+        * main
+        * declare class variable 'sol'
+        * call 'multTable' to print multiplication table
+    */
+    /*
+     * JUnit 5
+     * nothing to test? just printing a table
+     */
+    // multiplication table method
+    public void multTable(){
+    }
+
+    // main
+    public static void main(String[] args) {
+
+    }
+}
