e37bebc8384ed3eb399d4e070adab8ba57ec374b
Roesn <Bubwub04@icloud.com>
2021-10-04 00:02:17 -0400

    ex39

--- exercise39/src/main/java/Solution39.java
+++ exercise39/src/main/java/Solution39.java
@@ -3,15 +3,11 @@
  *  Copyright 2021 Trae Roy
  */
 
-import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
 
 public class Solution39 {
     /*
@@ -37,59 +33,92 @@ public class Solution39 {
     */
 
     public static void main(String[] args) {
-  
-        }
+        Employee[] employees = {
+                new Employee("John","Johnson","Manager",20161231),
+                new Employee("Tou","Xiong","Software Engineer",20161005),
+                new Employee("Michaela", "Michaelson", "District Manager", 20151219),
+                new Employee("Jake","Jacobson","Programmer", 0),
+                new Employee("Jackquelyn", "Jackson", "DBA", 0),
+                new Employee("Sally","Webber","Web Developer",20151218)
+        };
+
+        // get List view of the Employees
+        List<Employee> list = Arrays.asList(employees);
+
+        // display all Employees
+        System.out.println("Complete Employee list:");
+        list.forEach(System.out::println);
+
+        // Functions for getting first and last names from an Employee
+        Function<Employee, String> byFirstName = Employee::getFirstName;
+        Function<Employee, String> byLastName = Employee::getLastName;
+
+        // Comparator for comparing Employees by first name then last name
+        Comparator<Employee> lastThenFirst = Comparator.comparing(byLastName).thenComparing(byFirstName);
+
+        // sort employees by last name, then first name
+        System.out.printf("%nEmployees in ascending order by last name then first:%n");
+        list.stream().sorted(lastThenFirst).forEach(System.out::println);
     }
-    //class for each employee
-    class Employee {
+}
+//class for each employee
+class Employee {
+    private String firstName;
+    private String lastName;
+    private String department;
+    private double separationDate;
 
-        // constructor
-        public Employee(String firstName, String lastName, String department,double separationDate) {
-
-        }
-
-        // set firstName
-        public void setFirstName(String firstName) {
-
-        }
-
-        // get firstName
-        public String getFirstName() {
-
-        }
-
-        // set lastName
-        public void setLastName(String lastName) {
-
-        }
-
-        // get lastName
-        public String getLastName() {
-
-        }
-
-
-        // set department
-        public void setDepartment(String department) {
-
-        }
-
-        // get department
-        public String getDepartment() {
-
-        }
-
-        public void setSeparationDate(double separationDate) {
-
-        }
-
-        // get seperation date
-        public double getSeparationDate() {
-
-        }
-
-        // return Employee info
-        public String toString() {
-
-        }
+    // constructor
+    public Employee(String firstName, String lastName, String department,double separationDate) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setDepartment(department);
+        setSeparationDate(separationDate);
     }
+
+    // set firstName
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // get firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // set lastName
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // get lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    // set department
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    // get department
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setSeparationDate(double separationDate) {
+        this.separationDate = separationDate;
+    }
+
+    // get seperation date
+    public double getSeparationDate() {
+        return separationDate;
+    }
+
+    // return Employee info
+    public String toString() {
+        return String.format("%-8s %-8s %s %8.0f", getFirstName(), getLastName(), getDepartment(),getSeparationDate());
+    }
+}
+
