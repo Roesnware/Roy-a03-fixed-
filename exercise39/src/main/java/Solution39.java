bbf6145ac4e21f3ac10eeb0a9c98cd283d3fa394
Roesn <Bubwub04@icloud.com>
2021-10-04 00:00:37 -0400

    ex39skele

---
+++ exercise39/src/main/java/Solution39.java
@@ -0,0 +1,95 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Solution39 {
+    /*
+    * main
+    * make employee class variable employees
+    * add employees
+    * fetch the array list to view all employees
+    * print the array
+    * function to retreive the first and last name form an employee
+    * comparator compare employees by first then last anem
+    * sort employees by last name
+    * make employee class for employees
+    * constructor
+    * method to set first name
+    * method to get first name
+    * method to set last name
+    * method to get last name
+    * method to set department name
+    * method to get department name
+    * method to set seperationdate
+    * method to get seperationdate
+    * method to return employee info
+    */
+
+    public static void main(String[] args) {
+  
+        }
+    }
+    //class for each employee
+    class Employee {
+
+        // constructor
+        public Employee(String firstName, String lastName, String department,double separationDate) {
+
+        }
+
+        // set firstName
+        public void setFirstName(String firstName) {
+
+        }
+
+        // get firstName
+        public String getFirstName() {
+
+        }
+
+        // set lastName
+        public void setLastName(String lastName) {
+
+        }
+
+        // get lastName
+        public String getLastName() {
+
+        }
+
+
+        // set department
+        public void setDepartment(String department) {
+
+        }
+
+        // get department
+        public String getDepartment() {
+
+        }
+
+        public void setSeparationDate(double separationDate) {
+
+        }
+
+        // get seperation date
+        public double getSeparationDate() {
+
+        }
+
+        // return Employee info
+        public String toString() {
+
+        }
+    }
