aae656450749a93bd51fb8734737912402071250
Roesn <Bubwub04@icloud.com>
2021-10-03 23:29:34 -0400

    ex37skele

---
+++ exercise37/src/main/java/Solution37.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution37 {
+    /*
+     * make constant scanner 'input'
+     * make input method to scan integer 'inputMethod'
+     * main
+     * declare class variable 'sol'
+     * declare special characater array list
+     * repeat with numbers and letters
+     * prompt for min length
+     * prompt for special char
+     * prompt for size
+     * run a for loop for each adding random characters form each repsective domain
+     * put into a new array and print secure password
+     */
+    /*
+     * JUnit 5
+     * testing not applicable no input is altered
+     */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private int inputMethod(String prompt){
+    }
+
+    //main
+    public static void main(String[] args) {
+    }
+}
