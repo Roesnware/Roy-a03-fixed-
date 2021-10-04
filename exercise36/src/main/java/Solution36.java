b1cae27032a014683d4f64261a43b413c07efeb2
Roesn <Bubwub04@icloud.com>
2021-10-03 23:12:45 -0400

    ex36skele

---
+++ exercise36/src/main/java/Solution36.java
@@ -0,0 +1,67 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution36 {
+    /*
+    * make constant scanner 'input'
+    * make constant global array list 'array'
+    * make input method to scan in strings
+    * make method to get min out of arraylist 'getMin'
+    * make method to get max out of arraylist 'getMax'
+    * make method to get average out of arraylist 'getAverage'
+    * make method to get standard deviation out of arraylist 'getSD'
+    * make output method
+    * main
+    * declare class variable 'sol'
+    * declare 'string' variable
+    * declare 'num' for value input variable
+    * while loop to continue scanning int and checking if they are integers
+    * print 'numbers' in array list
+    * calc 'min' using 'getMin'
+    * calc 'max' using 'getMax'
+    * calc 'avg' using 'getAverage'
+    * calc 'stddev' using 'getSD'
+    */
+    /*
+     * JUnit 5
+     * input is not altered no test cases
+     */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    private static ArrayList<Integer> array;
+
+    // input
+    private String inputMethod(){
+    }
+
+    // calc min
+    public int getMin() {
+    }
+
+    // calc max
+    public int getMax() {
+    }
+
+    // calc average
+    public double getAverage() {
+    }
+
+    // calc standard deviation
+    public double getSD() {
+    }
+
+    // output
+    public void output() {
+    }
+
+    // main
+    public static void main(String[] args) {
+    }
+}
+
