ec6776fa031f0ebe439997f1be5dec32a05af41c
Roesn <Bubwub04@icloud.com>
2021-10-03 21:25:54 -0400

    ex32

--- exercise32/src/main/java/Solution32.java
+++ exercise32/src/main/java/Solution32.java
@@ -46,21 +46,97 @@ public class Solution32 {
 
     // for difficulty input
     private String inputMethod(){
+        System.out.println("Enter the difficulty level(1, 2, or  3): ");
+        return input.next();
     }
 
     // for guesses input
     private String inputMethod2(){
+        return input.next();
     }
 
     // check if digits
     public boolean checkDigits(String str) {
+
+        try {
+            Integer.parseInt(str);
+        }catch(Exception e) {
+            return false;
+        }
+        return true;
     }
 
     // for redo input
     private char inputMethod3(){
+        System.out.println("\nDo you wish to play again (Y/N)?  ");
+        return input.next().charAt(0);
     }
 
     // main
     public static void main(String[] args) {
+
+        Solution32 sol = new Solution32();
+
+        System.out.println("Let's play Guess the Number!");
+        int randomNum;
+        String guess;
+        int guessNum;
+        int counter;
+        String key;
+        int difficulty;
+
+        while(true) {
+
+            counter = 0;
+
+            while (true) {
+
+                key = sol.inputMethod();
+
+                if (sol.checkDigits(key)) {
+                    difficulty = Integer.parseInt(key);
+                    break;
+                } else {
+                    System.out.println("Please enter a numeric value.");
+                }
+            }
+
+            if (difficulty == 1) {
+                randomNum = (int) (Math.random() * 10 + 1);
+            } else if (difficulty == 2){
+                randomNum = (int) (Math.random() * 100 + 1);
+            }else {
+                randomNum = (int) (Math.random() * 1000 + 1);
+            }
+
+            System.out.print("I have my number now. What's your guess? ");
+            do{
+                while(true){
+
+                    guess = sol.inputMethod2();
+
+                    if (sol.checkDigits(guess)){
+                        guessNum = Integer.parseInt(guess);
+                        counter++;
+                        break;
+                    }else{
+                        System.out.println("Please enter a numeric value.");
+                        counter++;
+                    }
+                }
+
+                if (guessNum == randomNum){
+                    System.out.printf("You got it in %d guesses!", counter);
+                }else if(guessNum > randomNum){
+                    System.out.println("Too high. Guess again: ");
+                }else{
+                    System.out.println("Too low. Guess again: ");
+                }
+            }while (guessNum != randomNum);
+
+            char again = sol.inputMethod3();
+            if(again == 'N' || again == 'n')
+                break;
+        }
     }
 }
--- exercise32/src/test/java/Solution32Test.java
+++ exercise32/src/test/java/Solution32Test.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class Solution32Test {
 
     @Test
-    void checkDigits() {
+    void checkDigitsTest() {
+        Solution32 sol = new Solution32();
+
+        String str = "122";
+        boolean ans = sol.checkDigits(str);
+        assertTrue(sol.checkDigits(str));
     }
 }
