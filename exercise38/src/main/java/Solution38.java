3b65f2582f62f9b892332b74655eed4cb7f6ccd0
Roesn <Bubwub04@icloud.com>
2021-10-03 23:41:00 -0400

    ex38skele

---
+++ exercise38/src/main/java/Solution38.java
@@ -0,0 +1,37 @@
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+public class Solution38 {
+    /*
+    * make constant scanner 'input'
+    * make input method to scan strings 'inputMEthod'
+    * make method to randomize password with given constraints
+    * main
+    * declare integer variables 'size' 'special' 'numbers' scan input using 'inputMethod'
+    * print output resulting password
+    */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private static String inputMethod(String prompt){
+    }
+
+    // make new random pass
+    public static String randPass(int length, int special, int nums) {
+    }
+
+    // main
+    public static void main(String[] args) {
+        
+    }
+}
+
+
