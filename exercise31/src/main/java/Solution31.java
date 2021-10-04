d551048eb253ec3f99d26e2564298bc4c0e3298e
Roesn <Bubwub04@icloud.com>
2021-10-03 20:12:09 -0400

    ex31skele

---
+++ exercise31/src/main/java/Solution31.java
@@ -0,0 +1,57 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.Scanner;
+
+public class Solution31 {
+    /*
+    * make constant scanner 'input'
+    * make string method to scan string input 'inputMethod'
+    * make boolean method that takes string
+        * if parse to int is successful string is integers, true
+        * otherwise, false
+    * make method to print intensity rate table
+        * declare idex variable
+        * for loop i = 55, starting, through 95, increment 5 each for percents
+            * declare integer variable rate calc rate using formula
+            * print ' 'i' tabtabtab 'rate'
+    * main
+    * declare class variable 'sol'
+    * while condition while 'pulse' is integer, check with 'checkDigits'
+        * parse to integer if valid
+        * otherwise print ' Invalid pulse. '
+     * while condition while 'age' is integer, check with 'checkDigits'
+        * parse to integer if valid
+        * otherwise print ' Invalid age. '
+    * print top of table print 'Intensity tab Rate'
+    * call 'getIntRate' to get table
+    */
+    /*
+     * JUnit 5
+     * checkDigits testing
+     * hard code string to be '65'
+     * set True to run test on method
+     * should be true
+     */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private String inputMethod(String prompt){
+    }
+
+    // check if digits
+    public boolean checkDigits(String str) {
+    }
+
+    // calc intensity and rate
+    public void getIntRate(int age, int pulse){
+    }
+
+    // main
+    public static void main(String[] args) {
+    }
+}
+
---
+++ exercise31/src/test/java/Solution31Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution31Test {
+
+    @Test
+    void checkDigitsTest() {
+
+    }
+}
