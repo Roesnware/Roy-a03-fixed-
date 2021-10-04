062bd818cbe9868a8172aafca3cf324e30960437
Roesn <Bubwub04@icloud.com>
2021-10-03 22:16:28 -0400

    ex35skele

---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,41 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution35 {
+    /*
+    * make constant scanner input
+    * make input method to scan in String inputMethod
+    * main
+    * declare class variable sol
+    * initialize string variable 'name'
+    * declare array list variable 'names'
+    * do-while loop to scan until empty space
+    * set 'names' to user input using 'inputMethod'
+    * add input 'name' to array list 'names'
+    * remove the empty space from index
+    * pick a random 'winner' in 'winner' variable
+    * print 'The winner is... 'winner' '
+    */
+    /*
+     * JUnit 5
+     * no method to test strictly random and input/output
+     */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private String inputMethod(){
+    }
+
+    // main
+    public static void main(String[] args) {
+
+    }
+}
