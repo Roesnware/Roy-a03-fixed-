d108bbcfe8919bc795628cd8eb96f336980ca186
Roesn <Bubwub04@icloud.com>
2021-10-03 21:44:52 -0400

    ex33skele

---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution33 {
+    /*
+    * make constant scanner input
+    * make String method to scan input inputMethod
+    * main
+    * declare class variable sol
+    * declarer random number variable rand
+    * put responses in an output array
+    * prompt for question
+    * declare integer variable index to select random response in output array
+    * print output
+    * print new line
+    */
+    /*
+    * JUnit 5
+    * no methods to unit test all input and randomness
+    */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private String inputMethod(){
+
+    }
+
+    // main
+    public static void main(String[] args) {
+
+    }
+}
+
