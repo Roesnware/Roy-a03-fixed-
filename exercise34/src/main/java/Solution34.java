cffabf2a8bbce33a5af9e61430196634ecd4e086
Roesn <Bubwub04@icloud.com>
2021-10-03 22:01:21 -0400

    ex34skele

---
+++ exercise34/src/main/java/Solution34.java
@@ -0,0 +1,41 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Trae Roy
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution34 {
+    /*
+    * make constant scanner 'input'
+    * make String method 'inputMethod' to scan String input
+    * make void method 'outputMethod' to print the remaining string of employees
+    * main
+    * make class variable 'sol'
+    * declare Array list variable employees
+    * print starting employees
+    * prompt for which employee to remove
+    * print new line
+    * print output
+    */
+    /*
+    * JUnit 5
+    * no method unit to test all input/output based
+    */
+    // constant scanner
+    private static final Scanner input = new Scanner(System.in);
+
+    // input
+    private String inputMethod(){
+    }
+
+    // print output
+    public void outputMethod(ArrayList<String> employees) {
+    }
+    
+    // main
+    public static void main(String[] args) {
+    }
+}
